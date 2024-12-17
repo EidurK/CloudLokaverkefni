2709b0bd84cedab82f6eb73878568c0493ac0769
EidurK <ek040803@gmail.com>
2024-12-17 18:24:19 +0000

    hyperparameter tuning

--- src/main.py
+++ src/main.py
@@ -10,11 +10,11 @@ import pandas as pd
 # ------------------------------
 input_file = Path("../data/FullyCleanedDataframe.csv")
 
-min_df = 0.05
-max_df = 0.2
-max_features = 200
+min_df = 0.01
+max_df = 0.1
+max_features = 1000
 
-topic_count = 15
+topic_count = 10
 word_count_per_group = 8
 
 
@@ -26,6 +26,7 @@ df = pd.read_csv(input_file, encoding='utf-8')
 df = df.dropna()
 D = df['text'].to_numpy()
 X, vocab = NMF.tfid_vectorizer(D,min_df,max_df, max_features=max_features)
+print("vocabulary size:", vocab.shape[0])
 W,H = NMF.nmf(X,topic_count)
 
 top_words = NMF.top_n_words(H,word_count_per_group, vocab)
